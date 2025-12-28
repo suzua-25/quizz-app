5cafa474d43d8d1e5c3f9343a32d266cc4340806
suzua-25 <pyon.la2i.s@gmail.com>
2025-12-28 19:32:01 +0900

    superbase.js環境変数対応

--- src/supabase.js
+++ src/supabase.js
@@ -1,8 +1,13 @@
 import { createClient } from '@supabase/supabase-js'
 
 // ★★★ ここに先ほどコピーした情報を貼り付け ★★★
-const supabaseUrl = 'https://ecenvhtxhjqtpcxhpjfe.supabase.co/'
-const supabaseAnonKey = 'sb_publishable_hzKU2bA44_xxIDiuLsaN0w_GBgHLQED'
+// const supabaseUrl = 'https://ecenvhtxhjqtpcxhpjfe.supabase.co/'
+// const supabaseAnonKey = 'sb_publishable_hzKU2bA44_xxIDiuLsaN0w_GBgHLQED'
+
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL || 'https://ecenvhtxhjqtpcxhpjfe.supabase.co/'
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY || 'https://ecenvhtxhjqtpcxhpjfe.supabase.co/'
+
+
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
